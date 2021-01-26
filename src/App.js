c0d895e2db6e6aaa1879fba0a4c36df4066c4c01
JackyLin2 <jacky.jia.lin@gmail.com>
2021-01-25 23:37:27 -0500

    testing api

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Note from './components/Notes'
+import Api from './Api'
 
 function App() {
+
   return (<div>
       <Header/>
-      <Note/>
+
+     {Api.map(statements => <Note 
+        key = {statements.id}
+        content = {statements.content}
+        title = {statements.title}
+     /> )} 
       <Footer/>
   </div>
   );
